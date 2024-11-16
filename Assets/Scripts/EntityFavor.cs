a309b13f2f69a52a61ef25de653ff2309eff9259
JacobLLeBlanc <jacob.leblanc@my.utsa.edu>
2024-11-15 19:34:21 -0600

    Changed EntityFavor to allow the modification of the favor variable.

--- Assets/Scripts/EntityFavor.cs
+++ Assets/Scripts/EntityFavor.cs
@@ -8,23 +8,39 @@ public class EntityFavor : MonoBehaviour
     [Header("Entity Script Values")]
     [SerializeField] protected int maxFavor = 100; // Maximum Favor value
     [SerializeField] protected int currentFavor = 0;   // Current Favor value
-    
-    void Start()
-    {
-        
-    }
 
-     // Method to take damage
-     public void takeDamage(int amount)
+    // Method to modify damage by addition / subtraction
+    public void addFavor(int amount)
     {
-        currentFavor += amount; // Subtract damage from current health
+        currentFavor += amount;
         if (currentFavor < 0)
         {
-            currentFavor = 0; // Prevent negative health
+            currentFavor = 0;
+        }
+        if (currentFavor > maxFavor)
+        {
+            currentFavor = maxFavor;
         }
 
-        Debug.Log($"Took damage: {amount}. Current Favor: {currentFavor}");
+        Debug.Log($"Modified favor by: {amount}. Current Favor: {currentFavor}");
+    }
+    public void setFavor(int amount)
+    {
+        currentFavor = amount;
+        if (currentFavor < 0)
+        {
+            currentFavor = 0;
+        }
+        if (currentFavor > maxFavor)
+        {
+            currentFavor = maxFavor;
+        }
 
+        Debug.Log($"Set favor to: {amount}. Current Favor: {currentFavor}");
+    }
+
+    public void maxOutFavor() {
+        currentFavor = maxFavor;
     }
 
     public int getFavor(){
@@ -34,19 +50,4 @@ public class EntityFavor : MonoBehaviour
     public int getMaxFavor(){
         return maxFavor;
     }
-
-    // Method to heal
-    public void GainFavor(int amount)
-    {
-        currentFavor += amount; // Add healing amount to current health
-        if (currentFavor > maxFavor)
-        {
-            currentFavor = maxFavor; // Prevent exceeding max Favor
-        }
-
-        Debug.Log($"GainedFavor: {amount}. Current Favor: {currentFavor}");
-    }
-
-    // Method to get current health
-
 }
